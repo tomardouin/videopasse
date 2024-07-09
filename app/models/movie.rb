bd51f0e547b17461bea3f66b01afc2b7f170f816
Valentin Erhard <valentin.erhard@gmail.com>
2024-07-09 16:43:05 +0200

    ajout logo mise en place de la recherche par titre, ajout

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+                against: [:title],
+                using: {
+                  tsearch: { prefix: true }
+                }
     has_many :bookmarks
     has_many :offers
 

e6f99e737a5b34343caab35acb3617a5f65df8d8
Valentin Erhard <valentin.erhard@gmail.com>
2024-07-10 14:46:10 +0200

    modif db, et call api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_144309) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_101144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,13 +25,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_144309) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.string "overview"
     t.string "poster_url"
-    t.string "categorie_name"
     t.boolean "status", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "release_year"
+    t.integer "genre_ids", default: [], array: true
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+@user = User.create(username: 'test', email: 'test@testmail.com', password: '123456')
+Movie.destroy_all
+apiurl = "https://tmdb.lewagon.com/movie/top_rated"
+top_rated = URI.open(apiurl).read
+movies = JSON.parse(top_rated)
+
+movies["results"].each do |m|
+  poster_path = m["poster_path"]
+  Movie.create!(title: m["title"],
+    overview: m["overview"],
+    release_year: m["release_year"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{poster_path}",
+    user: User.first
+  )
+end
+puts "tout va bien"
